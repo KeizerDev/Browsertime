c697ee61ee8c1264eaf0cf1d5c0d4c26680a286a
Robert-Jan Keizer <robertjankeizer@gmail.com>
2016-01-15 11:01:33 +0100

    Added dummy player

--- client/routes/MovieId.js
+++ client/routes/MovieId.js
@@ -31,29 +31,27 @@ var Movie = React.createClass({
         }
 
         var client = new WebTorrent()
-        var magnetURI = "magnet:?xt=urn:btih:A2EEF86438B9FEAF95A6CB95D1895695832FDB7C&dn=sisters+2015+720p+hdrip+x264+ac3+zi+t&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce";   
 
-        client.add(magnetURI, onTorrent)
+        var torrentId = 'magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d'
+
+        client.add(torrentId, onTorrent)
 
         function onTorrent (torrent) {
-            var torrentFileName = path.basename(torrent.name, path.extname(torrent.name)) + '.torrent'
+            // var torrentFileName = path.basename(torrent.name, path.extname(torrent.name)) + '.torrent'
 
-            util.log(
-                '<a class="btn btn-primary btn-xs" href="' + torrent.magnetURI + '" role="button"><i class="fa fa-magnet"></i> Magnet URI</a> ' +
-                '<a class="btn btn-primary btn-xs" href="' + torrent.torrentFileURL + '" target="_blank" download="' + torrentFileName + '" role="button"><i class="fa fa-download"></i> Download .torrent</a> ' +
-                '<a id="downloadButton" class="btn btn-primary btn-xs" role="button"><i class="fa fa-download"></i> Download ' + torrent.name + '</a>'
-            )
+            // '<a class="btn btn-primary btn-xs" href="' + torrent.magnetURI + '" role="button"><i class="fa fa-magnet"></i> Magnet URI</a> ' +
+            // '<a class="btn btn-primary btn-xs" href="' + torrent.torrentFileURL + '" target="_blank" download="' + torrentFileName + '" role="button"><i class="fa fa-download"></i> Download .torrent</a> ' +
+            // '<a id="downloadButton" class="btn btn-primary btn-xs" role="button"><i class="fa fa-download"></i> Download ' + torrent.name + '</a>'
 
             function updateSpeed () {
                 var progress = (100 * torrent.progress).toFixed(1)
-                util.updateSpeed(
-                '<b>Peers:</b> ' + torrent.swarm.wires.length + ' ' +
-                '<b>Progress:</b> ' + progress + '% ' +
-                '<b>Download speed:</b> ' + prettyBytes(client.downloadSpeed()) + '/s ' +
-                '<b>Upload speed:</b> ' + prettyBytes(client.uploadSpeed()) + '/s'
-                )
-                progressBar.setAttribute('aria-valuenow', progress)
-                progressBar.setAttribute('style', 'width: ' + progress + '%')
+                console.log(progress)
+                // '<b>Peers:</b> ' + torrent.swarm.wires.length + ' ' +
+                // '<b>Progress:</b> ' + progress + '% ' +
+                // '<b>Download speed:</b> ' + prettyBytes(client.downloadSpeed()) + '/s ' +
+                // '<b>Upload speed:</b> ' + prettyBytes(client.uploadSpeed()) + '/s'
+                // progressBar.setAttribute('aria-valuenow', progress)
+                // progressBar.setAttribute('style', 'width: ' + progress + '%')
             }
 
             updateSpeed()
@@ -63,24 +61,24 @@ var Movie = React.createClass({
                 // Create a video element
                 file.appendTo('#player')
 
-                downloadButton.addEventListener('click', function () {
-                    var download = document.getElementById('download')
-                    download.classList.remove('hidden')
+                // downloadButton.addEventListener('click', function () {
+                //     var download = document.getElementById('download')
+                //     download.classList.remove('hidden')
 
-                    // Get a url for each file
-                    file.getBlobURL(function (err, url) {
-                        if (err) return util.error(err)
+                //     // Get a url for each file
+                //     file.getBlobURL(function (err, url) {
+                //         if (err) return util.error(err)
 
-                        // Hide download progress
-                        download.classList.add('hidden')
+                //         // Hide download progress
+                //         download.classList.add('hidden')
 
-                        // Add a link to the page
-                        var a = document.createElement('a')
-                        a.download = window.URL.createObjectURL(url)
-                        a.click()
-                        window.URL.revokeObjectURL(url)
-                    })
-                })
+                //         // Add a link to the page
+                //         var a = document.createElement('a')
+                //         a.download = window.URL.createObjectURL(url)
+                //         a.click()
+                //         window.URL.revokeObjectURL(url)
+                //     })
+                // })
             })
         }
     },
