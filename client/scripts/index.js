b40f0577665e06c484406edd4c4e525bf9ed27a4
bobbiy <bobbie.rausch@me.com>
2016-01-15 21:05:14 +0100

    Added search route. I didn't implement a search box yet. But you could search by browsing to /search/ + your query

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -18,7 +18,7 @@ var Footer = React.createClass({
 	render: function() {
 		return (<footer className="mdl-mini-footer">
 		<div className="mdl-mini-footer__left-section">
-		Made with a bunch of ♥ || Support this project: 146z8MPUeogNxpT3ptpBWjr6rkpviGd4JG
+			<div className="courier-new-font">Support this project: 146z8MPUeogNxpT3ptpBWjr6rkpviGd4JG</div>
 		</div>
 	</footer>);
 	}
@@ -32,6 +32,7 @@ var PageNav = React.createClass({
 					<ul>
 						<li><Router.Link to="movies">Movies</Router.Link></li>
 						<li><Router.Link to="series">TV Series</Router.Link></li>
+						<li><i className="material-icons">search</i></li>
 					</ul>
 				</div>
 			</div>
@@ -44,9 +45,7 @@ var App = React.createClass({
 		return (
 			<div className="">
 				<Header />
-				<div className="body-container">
-					<Router.RouteHandler/>
-				</div>
+				<Router.RouteHandler/>
 				<Footer/>
 			</div>
 		);
@@ -57,15 +56,18 @@ var routes = {
 	Home: require('../routes/Home'),
 	Movie: require('../routes/Movie'),
 	MovieId: require('../routes/MovieId'),
+	MovieQuery: require('../routes/MovieQuery'),
 	Series: require('../routes/Serie')
 };
 
 var routes = (
 	<Router.Route name="app" path="/" handler={App}>
+		<Router.Route name="home" path="/" handler={routes.Home}/>
 		<Router.Route name="movies" path="/movies" handler={routes.Movie}/>
 		<Router.Route name="movieid" path="/movies/:id" handler={routes.MovieId}/>
 		<Router.Route name="series" path="/series" handler={routes.Series}/>
-		<Router.DefaultRoute handler={routes.Movie}/>
+		<Router.Route name="moviequery" path="/search/:query" handler={routes.MovieQuery}/>
+		<Router.DefaultRoute handler={routes.Home}/>
 	</Router.Route>
 );
 
